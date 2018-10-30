e1f9f6a4984ab7eca6dc0ef72817b25f8b1ae6c9
ExplodingPotato <wojtek4p4@gmail.com>
2018-10-30 12:43:35 +0100

    Plz work again

---
+++ AUTG_Graph/ViewModels/GraphViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AUTG_Graph.ViewModels
+{
+	class GraphViewModel
+	{
+		private Graph graph;
+
+		public void GenerateRandom(int vertCount, float edgeChance, int maxWeight = 1)
+		{
+			graph = new Graph();
+		}
+
+		public int[,] GetGraphMatrix()
+		{
+			if (graph == null || graph.Matrix == null)
+				return null;
+
+			else
+				return graph.vertMatrix;
+		}
+    }
+}
