38dba303f4c4e993623ec8c20b331a0278bfdce7
ExplodingPotato <wojtek4p4@gmail.com>
2018-12-02 16:37:35 +0100

    Added method Edges to the Graph class that returns indexes of starts and ends of every edge

--- AUTG_Graph/Model/Graph.cs
+++ AUTG_Graph/Model/Graph.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AUTG_Graph.Model
 {
 	class Graph
 	{
 		private Random rand = new Random();
-		
+
 		public bool[,] NMatrix { get; private set; }
 		public uint Size { get; private set; }
 
@@ -30,6 +27,22 @@ namespace AUTG_Graph.Model
 					NMatrix[i, j] = NMatrix[j, i] = rand.NextDouble() <= edgeChance;
 				}
 			}
+		}
+
+		public (int, int)[] Edges()
+		{
+			List<(int, int)> returnList = new List<(int, int)>();
+
+			for (int i = 0; i < Size; i++)
+			{
+				for (int j = i + 1; j < Size; j++)
+				{
+					if (NMatrix[i, j])
+						returnList.Add((i, j));
+				}
+			}
+
+			return returnList.ToArray();
 		}
 	}
 }
